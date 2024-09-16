7e0f53351e626d1cb61fc2540305da127ee848bd
Codkashh <as3733542@gmail.com>
2024-09-16 16:50:01 +0530

    Auto refreshing hhook

--- Week 9/Custom-Hook/src/App.jsx
+++ Week 9/Custom-Hook/src/App.jsx
@@ -1,27 +1,53 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function useTodos(n) {
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState([])
+
+  function getData() {
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      })
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      getData();
+    }, n * 1000)
+    getData();
+  }, [n])
+
+  return {
+    todos: todos,
+    loading: loading
+  };
+}
 
 function App() {
-  return(
-    <div>
-      <MyComponent />
+  const { todos, loading } = useTodos(5);
+
+  if (loading) {
+    return <div>
+      Loading...
     </div>
+  }
+
+  return (
+    <>
+      {todos.map(todo => <Track todo={todo} />)}
+    </>
   )
 }
 
-function MyComponent() {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={incrementCount}>Increment</button>
-    </div>
-  );
-} 
-
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
+}
 
 export default App
