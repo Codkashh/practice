21c39cc20cf0688c577d69fe37a836b14423f1c3
Codkashh <as3733542@gmail.com>
2025-01-16 21:08:34 +0530

    input validation using zod

--- Week-7-cohort3/Week-7.1/index.js
+++ Week-7-cohort3/Week-7.1/index.js
@@ -1,20 +1,44 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const { z } = require("zod");
 const JWT_SECRET = "akash"
-mongoose.connect("mongodb+srv://aakashsharma162000:9Mwbou3ZNxS4x00z@cluster0.tld8z.mongodb.net/todo-app");
+mongoose.connect("mongodb+srv://aakashsharma162000:9Mwbou3ZNxS4x00z@cluster0.tld8z.mongodb.net/todo-app?retryWrites=true&w=majority").then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+});
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async function(req, res) {
+    const requiredBody = z.object({
+        email: z.string(),
+        password: z.string(),
+        name: z.string()
+    })
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            message:"Incorrect format",
+            error:parsedDataWithSuccess.error
+        })
+    }
+
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
+    const hashedPassword = await bcrypt.hash(password,5);
+
     await UserModel.create({
         email: email,
-        password: password,
+        password: hashedPassword,
         name: name
     });
     
@@ -30,13 +54,20 @@ app.post("/signin", async function(req, res) {
 
     const response = await UserModel.findOne({
         email: email,
-        password: password,
     });
 
-    if (response) {
+    if(!response){
+        res.status(403).json({
+            message:"User doesn't exist"
+        })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, response.password);
+
+    if (passwordMatch) {
         const token = jwt.sign({
             id: response._id.toString()
-        }, JWT_SECRET)
+        }, JWT_SECRET);
 
         res.json({
             token
